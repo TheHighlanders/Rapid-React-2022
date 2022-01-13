d82597aa852837a24f5a0990d0523a2222f19dba
Adriana Massie <adrianammassie@gmail.com>
2022-01-13 15:28:09 -0500

    Fixed import errors in driveCMD

--- RapidReact/src/main/java/frc/robot/commands/driveCMD.java
+++ RapidReact/src/main/java/frc/robot/commands/driveCMD.java
@@ -5,7 +5,8 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.subsystems.drivetrain;
+import frc.robot.OI;
+import frc.robot.subsystems.DriveTrain;
 
 public class driveCMD extends CommandBase {
   /** Creates a new driveCMD. */
