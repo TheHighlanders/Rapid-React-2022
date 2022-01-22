ed9f4c3f307ca0924d7323092533254c9840efaa
cBriarwood <92546742+cBriarwood@users.noreply.github.com>
2022-01-22 14:27:27 -0500

    Added color sensor

--- RapidReact/src/main/java/frc/robot/commands/inTakeIn.java
+++ RapidReact/src/main/java/frc/robot/commands/inTakeIn.java
@@ -6,15 +6,27 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
 import frc.robot.subsystems.intake;
+import com.revrobotics.ColorSensorV3;
+import com.revrobotics.ColorMatch;
+import com.revrobotics.ColorMatchResult;
 
 public class inTakeIn extends CommandBase {
   /** Creates a new inTakeIn. */
   public final intake m_intake;
+  private final I2C.Port i2cPort = I2C.Port.kOnboard;
+  private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
+  private final ColorMatch m_colorMatcher = new ColorMatch();
   
   public inTakeIn(intake intake_subsystem) {
     m_intake = intake_subsystem;
     addRequirements(m_intake);
+
+    
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
@@ -25,6 +37,10 @@ public class inTakeIn extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    Color detectedColor = m_colorSensor.getColor();
+    double IR = m_colorSensor.getIR();
+    SmartDashboard.putNumber("Red", detectedColor.red);
+    SmartDashboard.putNumber("Blue", detectedColor.blue);
     m_intake.IntakeIn();
   }
 
